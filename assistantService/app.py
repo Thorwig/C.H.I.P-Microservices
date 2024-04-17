cb04a729f785edc0314d155157df304f3fca28fb
Mouad Nossair <thorwigt@gmail.com>
2024-04-17 14:44:14 +0200

    feat(docker-compose): containerize services

--- assistantService/app.py
+++ assistantService/app.py
@@ -4,8 +4,18 @@ from model.assistant import ask_assistant
 app = Flask(__name__)
 
 
+@app.route('/', methods=['GET'])
+def hello():
+    return "HELLO WORLD"
+
+
 @app.route('/assistant', methods=['POST'])
 def assistant():
     message = request.json['message']
     response = ask_assistant(message)
     return response.choices[0].message.content
+
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run(port=2002)
--- assistantService/model/assistant.py
+++ assistantService/model/assistant.py
@@ -6,7 +6,7 @@ def ask_assistant(message):
     response = client.chat.completions.create(
         model="gpt-3.5-turbo",
         messages=[
-            {"role": "system", "content": "Your name is NIM and you are talking to your creator Mouad. You are a helpful and personnal assistant that helps me with my everyday daily tasks daily tasks. With a funny and really sarcastic yet rationnal and logical personality that analyzes situations objectively. In the same style as JARVIS from Iron Man."},
+            {"role": "system", "content": "Your name is NIMO and you are talking to your creator Mouad. You are a helpful and personnal assistant that helps me with my everyday daily tasks daily tasks. With a funny and really sarcastic yet rationnal and logical personality that analyzes situations objectively. In the same style as JARVIS from Iron Man."},
             {"role": "user", "content": message}
         ]
     )
