e2c4fd5b9965fae4de8147ed702347253f87b9a3
Thorwig <thorwigt@gmail.com>
2024-04-30 15:11:39 +0200

    feat(stt): add temporary sockets for audio

--- sttService/app/__init__.py
+++ sttService/app/__init__.py
@@ -1,10 +1,15 @@
 from flask import Flask
+from flask_socketio import SocketIO
+
+socketio = SocketIO()
 
 
-def create_app():
+def create_app(debug=True):
     app = Flask(__name__)
+    app.debug = debug
 
     from .routes import api_bp
     app.register_blueprint(api_bp)
 
+    socketio.init_app(app)
     return app
--- sttService/app/routes.py
+++ sttService/app/routes.py
@@ -1,11 +1,31 @@
 from flask import Blueprint, request, jsonify
+from flask_socketio import emit
+from ..run import socketio
 from .model.stt import transcribe_audio
 
 api_bp = Blueprint('api_bp', __name__)
 
+
+@api_bp.route('/', methods=['GET'])
+def index():
+    return "HELLO WORLD, sttService"
 
 @api_bp.route('/stt', methods=['POST'])
 def stt():
     message = request.json['message']
     response = transcribe_audio(message)
     return jsonify({"audio_url": f"/audio/{response.id}"})
+
+
+@socketio.on('message')
+def handle_message(message):
+    app.logger.info('Received message')
+    # Process audio data here
+    transcription = transcribe_audio(message)
+    emit('transcription', {'text': transcription})
+
+
+@socketio.on('transcribe', namespace='/stt')
+def transcribe(message):
+    response = transcribe_audio(message['audio'])
+    emit('transcribe', {"audio_url": f"/audio/{response.id}"})
